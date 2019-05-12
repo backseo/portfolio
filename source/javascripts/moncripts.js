20d89f68eb64206b7573f561fe4e1d15b9937f04
serge cleoron <sc21417@gmail.com>
2019-05-12 03:53:22 +0200

    refonte 2019

--- source/javascripts/moncripts.js
+++ source/javascripts/moncripts.js
@@ -1,20 +1,41 @@
 $(document).ready(function() {
 
-  $(".imgover").hover(
+ $(".imgover").hover(
     function(){ $(this).addClass('img-m-over') },
     function(){ $(this).removeClass('img-m-over') }
  )
 
-  $(".img").hover(
+ $(".img").hover(
     function(){ $(this).addClass('img-m-over') },
     function(){ $(this).removeClass('img-m-over') },
-    );
+ );
 
+/* c'est quoi ce truc ?*/
     let cible = $('.a-wrapper-titles ul li');
       cible.on('click', function(){
       $(this).next('.a-container').toggleClass('hider');
     });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
